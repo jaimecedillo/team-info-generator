c03aa04afb1e91708b6c34fa24064f8d02979b3e
jaime <jaime@MacBook-Pro.local>
2021-03-28 09:23:39 -0600

    tested INterns properties and methods

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,16 @@
 const Intern = require('../lib/Intern');
+
+test('Intern school value', () => {
+    const intern = new Intern('John', 353, 'test@test.com', 'USC');
+    expect(intern.school).toBe('USC');
+})
+
+test('Get school value', () => {
+    const intern = new Intern('John', 353, 'test@test.com', 'USC');
+    expect(intern.getSchool()).toBe('USC');
+})
+
+test('GEt all of Interns info', () => {
+    const intern = new Intern('John', 353, 'test@test.com', 'USC');
+    expect(intern.getRole()).toBe('Intern');
+})
